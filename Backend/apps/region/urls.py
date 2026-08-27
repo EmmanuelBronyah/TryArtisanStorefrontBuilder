7b5935003345b4e6983f33785144d47a5a855147
Emmanuel Bronyah <emmanuelbronyah7@gmail.com>
2026-08-27 21:47:40 +0000

    fix typo

--- Backend/apps/region/urls.py
+++ Backend/apps/region/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from apps.region.views import ListRegionsAPIView
 
 urlpatterns = [
-    path("all/", ListRegionsAPIView.as_view(), name="all-crafts"),
+    path("all/", ListRegionsAPIView.as_view(), name="all-regions"),
 ]
